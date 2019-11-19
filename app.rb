61961a8da5d304d99b8cd618964bd9d70928fafa
Monica R <mlisareyes@gmail.com>
2019-11-19 03:26:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Lisa"
+  end
+
+  get '/hometown' do
+    "My hometown is Belleville."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Don't Panic by Coldplay."
+  end
 end
